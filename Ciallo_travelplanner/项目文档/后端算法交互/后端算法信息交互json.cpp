ef722790bfdafc6cb9329e4d3b6add02200aaae0
pompeiiking <22271226080@qq.com>
2025-05-10 16:06:42 +0800

    Ciallo_travelplanner 主项目文件

---
+++ Ciallo_travelplanner/项目文档/后端算法交互/后端算法信息交互json.cpp
@@ -0,0 +1,82 @@
+{
+	"trip_requirements":{
+		"total_score_route_info": {
+			"total_score":,
+            "start_point": "",
+            "end_point": "",
+            "route_distance":,
+            "attractions_passed by":[
+            "天坛" ,
+			"地坛" , 
+			], 
+			"attraction_attributes":[
+			"自然风光",
+            "历史人文",
+			],
+			"activity":[
+			"划船"
+			], 
+			"total_activity_cost":,
+        }
+    },
+    	"trip_requirements":{
+		"short_route_info": {
+			"distance_score":,
+            "start_point": "",
+            "end_point": "",
+            "route_distance":,
+            "attractions_passed by":[
+            "天坛" ,
+			"地坛" , 
+			], 
+			"attraction_attributes":[
+			"自然风光",
+            "历史人文",
+			],
+			"activity":[
+			"划船"
+			], 
+			"total_activity_cost":,
+        }
+    },
+    	"trip_requirements":{
+		"cheap_route_info": {
+			"cost_score":,
+            "start_point": "",
+            "end_point": "",
+            "route_distance":,
+            "attractions_passed by":[
+            "天坛" ,
+			"地坛" , 
+			], 
+			"attraction_attributes":[
+			"自然风光",
+            "历史人文",
+			],
+			"activity":[
+			"划船"
+			], 
+			"total_activity_cost":,
+        }
+    },
+    	"trip_requirements":{
+		"preferences_route_info": {
+			"preferences_score":,
+            "start_point": "",
+            "end_point": "",
+            "route_distance":,
+            "attractions_passed by":[
+            "天坛" ,
+			"地坛" , 
+			], 
+			"attraction_attributes":[
+			"自然风光",
+            "历史人文",
+			],
+			"activity":[
+			"划船"
+			], 
+			"total_activity_cost":,
+        }
+    },
+}
