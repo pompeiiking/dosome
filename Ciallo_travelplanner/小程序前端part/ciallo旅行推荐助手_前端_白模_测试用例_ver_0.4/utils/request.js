1823d1e02d1ec99488154ca743797bdbf274d854
pompeiiking <22271226080@qq.com>
2025-05-16 22:27:40 +0800

    顶尖前端！！

---
+++ Ciallo_travelplanner/小程序前端part/ciallo旅行推荐助手_前端_白模_测试用例_ver_0.4/utils/request.js
@@ -0,0 +1,48 @@
+// 后端接口基础地址（局域网地址）
+const BASE_URL = 'http://192.168.25.154:1099'  // 请替换为您的电脑IP地址和端口
+
+// 封装请求方法
+const request = (url, data) => {
+  return new Promise((resolve, reject) => {
+    wx.showLoading({
+      title: '加载中...',
+    })
+    
+    wx.request({
+      url: BASE_URL + url,
+      method: 'POST',
+      data: data,
+      header: {
+        'content-type': 'application/json'
+      },
+      success: (res) => {
+        wx.hideLoading()
+        if (res.statusCode === 200) {
+          resolve(res.data)
+        } else {
+          wx.showToast({
+            title: '请求失败',
+            icon: 'none'
+          })
+          reject(res)
+        }
+      },
+      fail: (err) => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '网络错误',
+          icon: 'none'
+        })
+        reject(err)
+      }
+    })
+  })
+}
+
+// 导出请求方法
+module.exports = {
+  // 发送旅行规划请求
+  sendTravelPlan: (data) => {
+    return request('/api/recommend', data)
+  }
+} 
